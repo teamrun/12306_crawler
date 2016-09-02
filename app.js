7f2fc9c21e9c1b3941700ec091f624ddc5ba6a0b
teamrun <chenllos@163.com>
2016-09-02 23:34:30 +0800

    use correct schedual

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const schedule = require('node-schedule');
 
 // exec a job every 30 mins: sec min hour day month day-in-week
 // 整点和半点执行查询, 7点~23点(23~6系统维护)
-// var j = schedule.scheduleJob('0 */30 7-23 * *', crawler);
-var j = schedule.scheduleJob('0 * 7-22 * * *', () => {
-  console.log('i will run every 2mins, now is', (new Date()).getMinutes());
+var j = schedule.scheduleJob('0 */30 7-23 * *', () => {
+  setTimeout(crawler, Math.random() * 5 * 60 * 1000);
 });
